9c79c0951e256893554084789c231b898cffa26e
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-02-14 17:03:15 -0800

    Update echo-arr.sh

--- echo-arr.sh
+++ echo-arr.sh
@@ -20,3 +20,25 @@ declare -A array1=( ["a"]="apple" ["b"]="boy" )
 declare -A array2=( ["z"]="zeebra" ["x"]="xero" )
 
 print_keys array1 array2
+
+
+# Function to print key-value pairs from two associative arrays
+print_keys_and_values() {
+    declare -n array1=$1
+    declare -n array2=$2
+
+    echo "Array 1:"
+    for key in "${!array1[@]}"; do
+        echo "$key: ${array1[$key]}"
+    done
+
+    echo "Array 2:"
+    for key in "${!array2[@]}"; do
+        echo "$key: ${array2[$key]}"
+    done
+}
+
+declare -A array3=( ["a"]="apple" ["b"]="boy" )
+declare -A array4=( ["z"]="zeebra" ["x"]="xero" )
+
+print_keys_and_values array31 array4
