25b6043b167b3b8124cf2f5974da520eea1216a1
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-02-21 00:13:17 -0800

    Update load.sh

--- lib/load.sh
+++ lib/load.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$DIR/functions.sh"
+# source "$DIR/functions.sh"
+source "$DIR/manage-instance-profiles.sh"
 echo "Functions loaded into memory."
