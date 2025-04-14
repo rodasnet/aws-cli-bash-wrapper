26988579f97f75f3620d1d1e0636d2d436b6a0ec
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-04-14 07:18:01 -0700

    Update functions.sh

--- lib/functions.sh
+++ lib/functions.sh
@@ -59,56 +59,6 @@ filter_params() {
     print_params matched_params
 }
 
-# filter_params() {
-#     local -A user_params=$1
-#     local -A required_params=$2
-#     local -A optional_params=$3
-#     local -A matched_params
-
-#     # Check for single dash (-*) or double dash (--*) parameters
-#     # handling both short-form (-*) and long-form (--*) dash-based parameters.
-#     for key in "${!required_params[@]}"; do
-#         if [[ -n "${user_params[--${required_params[$key]}]}" ]]; then
-#             matched_params["--${required_params[$key]}"]="${user_params[--${required_params[$key]}]}"
-#         fi
-
-#         if [[ -n "${user_params[-$key]}" ]]; then
-#             matched_params["-$key"]="${user_params[-$key]}"
-#         fi
-#     done
-
-#     print_params matched_params
-# }
-
-library_test_filter_params() {
-    echo "Library method filter_params..."  # Debugging statement
-    local -A required_params=( ["i"]="id" ["j"]="json" ["r"]="required" ) # map of required parameters
-    local -A optional_params=( ["p"]="profile" ["o"]="other-option" ) # map of optional parameters
-    local -A user_params
-    local -A filtered_params
-
-    local -S 
-
-    filtered_params=$(filter_params $(fetch_user_paramsV2 "$@") required_params optional_params)
-
-    echo "Required params:"
-    echo "${filtered_params[@]}"
-}
-
-# library_test_copy() {
-#     echo "Library method filter_params..."  # Debugging statement
-#     local -A required_params=( ["i"]="id" ["j"]="json" ["r"]="required" ) # map of required parameters
-#     local -A optional_params=( ["p"]="profile" ["o"]="other-option" ) # map of optional parameters
-#     local -A user_params
-#     local -A filtered_params
-
-#     filtered_params=$(filter_params $(fetch_user_paramsV2 "$@") required_params optional_params)
-
-#     echo "Required params:"
-#     echo "${filtered_params[@]}"
-# }
-
-
 replace_string_values_example() {
     local json_string="$1"
     local -A params=$2
@@ -152,3 +102,37 @@ replace_json_values() {
     return 0
 }
 
+# filter_params() {
+#     local -A user_params=$1
+#     local -A required_params=$2
+#     local -A optional_params=$3
+#     local -A matched_params
+
+#     # Check for single dash (-*) or double dash (--*) parameters
+#     # handling both short-form (-*) and long-form (--*) dash-based parameters.
+#     for key in "${!required_params[@]}"; do
+#         if [[ -n "${user_params[--${required_params[$key]}]}" ]]; then
+#             matched_params["--${required_params[$key]}"]="${user_params[--${required_params[$key]}]}"
+#         fi
+
+#         if [[ -n "${user_params[-$key]}" ]]; then
+#             matched_params["-$key"]="${user_params[-$key]}"
+#         fi
+#     done
+
+#     print_params matched_params
+# }
+
+
+# library_test_copy() {
+#     echo "Library method filter_params..."  # Debugging statement
+#     local -A required_params=( ["i"]="id" ["j"]="json" ["r"]="required" ) # map of required parameters
+#     local -A optional_params=( ["p"]="profile" ["o"]="other-option" ) # map of optional parameters
+#     local -A user_params
+#     local -A filtered_params
+
+#     filtered_params=$(filter_params $(fetch_user_paramsV2 "$@") required_params optional_params)
+
+#     echo "Required params:"
+#     echo "${filtered_params[@]}"
+# }
