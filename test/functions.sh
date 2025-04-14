1db4add21d98eb610d10abc01ab16ea26e1c3adc
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-04-14 07:02:52 -0700

    Rename functions

--- test/functions.sh
+++ test/functions.sh
@@ -8,26 +8,21 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # and ensure that they work as expected.
 # They are not meant to be run directly, but rather called from the main script.
 
-test_replace_json_template_values_V2() {
-    echo "Running test test_replace_json_template_values_V2..."
+test_replace_json_values_V2() {
+    echo "Running test test_replace_json_values_V2..."
 
     local json_template="$DIR/files/json/example.json"
-    local params="name=Edward age=82 city=New York occupation=Engineer"
+    local params="name=Daniel age=40 city=New York occupation=Engineer"
     local result=$(replace_json_values $json_template $params)
     echo "Result: $result"
-    # Test with different values
-    # Uncomment the following line to test with different values
-    # replace_json_template_values $json_template name=Jane age=25 city=Los Angeles occupation=Designer
-    # Uncomment the following line to test with different values
-    # replace_json_values $json_template name=Andrew age=48 city=Buffalo occupation=Doctor
 }
 
-test_replace_json_template_values() {
-    echo "Testing replace_json_template_values..."
+test_replace_string_values_example() {
+    echo "Testing replace_string_values_example..."
     local json_string='{"name": "{{name}}", "age": {{age}}}'
     local -A params=( ["name"]="John Doe" ["age"]=30 )
 
-    local result=$(replace_json_template_values "$json_string" params)
+    local result=$(replace_string_values_example "$json_string" params)
     echo "Result: $result"
 }
 
