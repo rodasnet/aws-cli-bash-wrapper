a630af95b5506f11d219c88ae1c1fff6d4bbf239
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-04-14 09:49:51 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,3 +4,11 @@
 source ./test/load.sh
 
 test_replace_json_values
+
+library_test_method_fetch_user_params --json input.json --id 123 --other-option other.json
+echo ""
+echo "----------------------------------------"
+echo ""
+
+library_test_methods_fetch_user_params_and_filter_params --json input.json --id 123 --other-option other.json
+# library_test_methods_fetch_user_params_and_filter_params  --json input.json --name jane --alternet-option other.json
