bb007fd0b3907cd785590ef849c2cbfc1d702f65
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-04-16 23:32:42 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -6,5 +6,5 @@ source ./test/load.sh
 # Run the test function
 # library_test_function_serialize_user_params # WORKING
 # library_test_function_print_params          # WORKING
-library_test_function_fetch_user_params     # NOT WORKING
-# library_test_function_filter_params         # NOT WORKING
+# library_test_function_fetch_user_params     # WORKING
+library_test_function_filter_params         # NOT WORKING
