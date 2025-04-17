85e89867230df26ac343992c98f20eec1e946969
Daniel Rodas <daniel.rodas1@hotmail.com>
2025-04-16 22:43:57 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -12,19 +12,7 @@ source ./test/load.sh
 # --json input.json --id 123 --other-option other.json
 # library_test_function_fetch_user_params --json input.json --id 123 --other-option other.json
 
-
-# Test the library_test_function_filter_params function
-# Example usage of the function
-# library_test_function_filter_params
-# 
-# Expected output:
-# --json input.json --id 123 --other-option other.json
-# library_test_function_filter_params
-
-# library_test_function_filter_params --json input.json --id 123 --other-option other.json
-# # library_test_methods_fetch_user_params_and_filter_params --json input.json --id 123 --other-option other.json
-# library_test_methods_fetch_user_params_and_serialize_user_params --json input.json --id 123 --other-option other.json
-# test_replace_json_values
-
 # Run the test function
-library_test_function_serialize_user_params
+# library_test_function_serialize_user_params # WORKING
+# library_test_function_fetch_user_params     # NOT WORKING
+# library_test_function_filter_params         # NOT WORKING
